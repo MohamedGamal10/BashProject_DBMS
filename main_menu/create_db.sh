371bc132075ba3719651900edc8aa04e3e95162e
MohamedGamal10 <mohamedgamal2595@gmail.com>
2024-07-30 23:30:50 +0300

    Modify create_db file

--- main_menu/create_db.sh
+++ main_menu/create_db.sh
@@ -5,13 +5,20 @@ DB_ROOT="../databases"
 # Create Database
 function create_db {
     
+    #Read Database Name
     read -p "Enter database name: " db_name
+    #Check if there special chracter
+    while true
+    #Check if there a capital letters
+    #Check if database name already exist
     if [ -d "$DB_ROOT/$db_name" ]; then
 	echo " "
         echo "###################################"
 	echo "Database '$db_name' already exists."
 	echo "###################################"
-    else
+  
+    #Create Database
+     else
         mkdir -p "$DB_ROOT/$db_name"
         echo " "
         echo "###################################"
