5b7ae44c42b119b260340c085da0f52f9c86a643
MohamedGamal10 <mohamedgamal2595@gmail.com>
2024-08-01 13:11:03 +0300

    Add Display Creation Time

--- main_menu/list_db.sh
+++ main_menu/list_db.sh
@@ -4,10 +4,22 @@ DB_ROOT="../databases"
 
 function list_db {
      
-     #List Databases
-     echo "========================================"
-     echo "|Database_Name            Creation_Time|"
-     echo "========================================"
-     echo "$(ls -l $DB_ROOT | awk '{print $9}')"
-     
+    #List Databases
+    echo "========================================"
+    echo "| Database_Name           Creation_Time|"
+    echo "========================================"
+
+    if [ -d "$DB_ROOT" ]; then
+        while IFS= read -r line; do
+            db_name=$(echo "$line" | awk '{print $9}')
+            creation_time=$(echo "$line" | awk '{print $6, $7, $8}')
+            if [[ -n "$db_name" ]]; then
+                echo "| $db_name                   $creation_time|"
+            fi
+        done < <(ls -l "$DB_ROOT" | tail -n +2)
+    else
+        echo "| No databases found.                   |"
+    fi
+
+    echo "========================================"
 }
