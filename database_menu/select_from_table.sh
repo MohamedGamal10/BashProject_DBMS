b5653cbdb95aa07bd483b405850a0f2aade0f836
MohamedGamal10 <mohamedgamal2595@gmail.com>
2024-08-08 13:19:46 +0300

    Modify select from table module

--- database_menu/select_from_table.sh
+++ database_menu/select_from_table.sh
@@ -4,6 +4,33 @@ DB_ROOT="./databases"
 
 function select_from_table {
 	db_name=$1
-        echo "select_from_table '$db_name'"
+        
+	read -p "Enter table name: " table_name
+	
+    	meta_file="$DB_ROOT/$db_name/$table_name/$table_name.meta"
+    	data_file="$DB_ROOT/$db_name/$table_name/$table_name.data"
+
+	if [ ! -f "$meta_file" ]; then
+        	echo "Table '$table_name' does not exist."
+        	database_menu "$db_name"
+        	return
+    	fi
+
+	column_name=$(awk -F,  '{print $2}' "$meta_file" | cut -d: -f1)
+	read -p "Enter $column_name : " value
+
+	selection=$(grep "$value" "$data_file")
+	
+	if [ -n "$selection" ]; then
+    		echo "$selection"
+		echo "====================================="
+		echo "|$selection|"
+		echo "====================================="
+	else
+    		echo "empty Data"
+	fi
+
+	database_menu "$db_name"
+
 
 }
